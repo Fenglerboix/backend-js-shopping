1fdfdf537f4d2737c1eb2408184c74d3eb344e2b
Julian <julian@Ingeneo-MacBook-Pro.local>
2019-06-15 17:23:07 -0500

    shopping car with node git add .

--- routes.js
+++ routes.js
@@ -1,17 +1,15 @@
 /**
  * Main application routes
+ * @author: Cristian Moreno Zuluaga <khriztianmoreno@gmail.com>
  */
 
 // Import Endpoints
 const helloWorld = require('./api/helloworld');
+// New Line
+const product = require('./api/product');
 
 module.exports = (app) => {
-
-    // Insert routes below
     app.use('/api/helloworld', helloWorld);
-
-    // Next routes
-    // Endpoints in plural
-    // app.use('/api/users', user);
-    // app.use('/api/products', product);
+    // New line
+    app.use('/api/products', product);
 };
